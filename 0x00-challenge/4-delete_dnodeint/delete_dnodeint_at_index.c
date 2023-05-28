c84ec452c683bd9f01bff5562a51dcf6cdb454fa
root <brendajeptum@gmail.com>
2023-05-28 15:29:59 -0700

    Fixed delete_dnodeint

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -13,7 +13,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *saved_head;
 	dlistint_t *tmp;
-    dlistint_t *current;
 	unsigned int p;
 
 	if (*head == NULL)
@@ -21,33 +20,34 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	}
 	saved_head = *head;
-    current = saved_head;
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
-    p = 0;
-    while (p < index && current != NULL)
-    {
-        current = current->next;
-        p++;
-    }
-    if (p != index)
-    {
-        *head = saved_head;
-        return (-1);
-    }
-    if (current == NULL || current->next == NULL)
-        return (-1);
-    tmp = current->next;
-    current->next = tmp->next;
-    if (tmp->next != NULL)
-        tmp->next->prev = current;
-    free(tmp);
-    *head = saved_head;
-    return (1);
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+        free(*head);
+		*head = saved_head;
+	}
+	return (1);
 }
